01f3145ba1bb428469699bc713242d58975974fd
Tony Cao <tony@benchling.com>
2022-07-18 02:51:41 -0700

    start adding basic form

--- src/forms/MakeupForm.tsx
+++ src/forms/MakeupForm.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+import "./form.css";
 
 export interface MakeupFormProps {}
 
 export default function MakeupForm(props: MakeupFormProps): JSX.Element {
-  return <>Makeup!</>;
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+
+  function onSubmit(data: unknown): void {
+    alert(data);
+    navigate("/");
+  }
+
+  return (
+    <div className="makeup-form">
+      <div className="title">Add new product!</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="Name" {...register("name")} />
+        <input placeholder="URL" {...register("url")} />
+        <input placeholder="Price" type="number" {...register("Price")} />
+        <input
+          placeholder="Rating"
+          type="number"
+          {...register("Rating", { min: 0, max: 10 })}
+        />
+        <textarea
+          placeholder="Notes"
+          {...register("notes", { maxLength: 1000 })}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
